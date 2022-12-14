41450121058b89b4dcea8ba6e24699a6b9183042
Paulo Feodrippe <paulo.feodrippe@ip-192-168-2-13.ec2.internal>
2022-12-13 22:06:49 -0500

    more space

--- src/com/pfeodrippe/tooling/clerk.clj
+++ src/com/pfeodrippe/tooling/clerk.clj
@@ -7,35 +7,27 @@
 (defn view-index
   [sections]
   (with-meta
-    [:<>
-     [:p {:style {:border-top "solid black 0.25rem"
-                  :width "40rem"
-                  :x "-1rem"
-                  :y "-4rem"
-                  :position :absolute
-                  :left "-0rem"
-                  :margin-top "-20px"}}]
-     (into [:div {:style {:font-size "120%"}}]
-           (for [{:keys [title pages idx]}
-                 (->> sections (map-indexed (fn [idx m] (assoc m :idx idx))))]
-             [:<>
-              [:div.text-xl {:class (when-not (zero? idx) [:mt-8])}
-               [:b title]]
-              (when (seq pages)
-                [:ul.columns-2
-                 (for [child-page pages]
-                   (let [{:keys [location-name path error]}
-                         (tool.parser/xref-info-from-path child-page)]
-                     [:li.list-none.break-all {:style {:break-inside :avoid-column
-                                                       ;; We add to style as there is a rule that overrides
-                                                       ;; if we set these properties through tailwind.
-                                                       :margin 0
-                                                       :margin-bottom "0.6rem"}}
-                      [:a {:href path
-                           :class (cond-> tool.parser/link-classes
-                                    error (conj :bg-red-300))
-                           :style {:color "inherit"}}
-                       location-name]]))])]))]
+    (into [:div.pl-4 {:style {:font-size "120%"}}]
+          (for [{:keys [title pages idx]}
+                (->> sections (map-indexed (fn [idx m] (assoc m :idx idx))))]
+            [:<>
+             [:div.text-xl {:class (when-not (zero? idx) [:mt-8])}
+              [:b title]]
+             (when (seq pages)
+               [:ul.columns-2 {:style {:margin-top "5px"}}
+                (for [child-page pages]
+                  (let [{:keys [location-name path error]}
+                        (tool.parser/xref-info-from-path child-page)]
+                    [:li.list-none.break-all {:style {:break-inside :avoid-column
+                                                      ;; We add to style as there is a rule that overrides
+                                                      ;; if we set these properties through tailwind.
+                                                      :margin 0
+                                                      :margin-bottom "0.6rem"}}
+                     [:a {:href path
+                          :class (cond-> tool.parser/link-classes
+                                   error (conj :bg-red-300))
+                          :style {:color "inherit"}}
+                      location-name]]))])]))
     {:type `index-viewer}))
 
 (defn add-path-info!
