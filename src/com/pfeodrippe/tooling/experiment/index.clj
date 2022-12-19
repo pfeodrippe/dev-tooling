3fc80fb8234eeb2e5379ec0c3cb5125d4fd8d816
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2022-12-19 10:01:42 -0500

    update todo

--- src/com/pfeodrippe/tooling/experiment/index.clj
+++ src/com/pfeodrippe/tooling/experiment/index.clj
@@ -34,6 +34,49 @@
                  :browse true})
 
   ;; TODO:
-  ;; - [x] Make static app work again
+  ;; - [x] Divide text in multiple columns for asides
+  ;; - [x] Page title
+  ;; - [x] Subtitle
+  ;; - [x] Improve external link UI
+  ;; - [x] Internal link (xref)
+  ;;   - [x] Go back to last page
+  ;;   - [x] Static build
+  ;;   - [x] Uppercase
+  ;;   - [x] Can refer to a ns using keyword as well (ns metadata or maybe a EDN
+  ;;         file or even another ns that contains the mapping)?
+  ;; - [x] Fix absolute path issue
+  ;; - [x] Redirect
+  ;; - [-] Change ns automatically in the url when the user goes to another ns.
+  ;;       Not for now
+  ;; - [x] Show red for a xref that doesn't exist
+  ;; - [x] Index
+  ;;   - [x] Section header
+  ;;     - [x] Children
+  ;;   - [-] Section page (for the future)
+  ;; - [x] Fix loading glitch
+  ;; - [x] In the JVM, show background in red for a link if it does not exist
+  ;; - [x] Use free concourse font
+  ;;   - [x] Ordered lists
+  ;;   - [x] Unordered lists
+  ;; - [x] Support md files
+  ;;   - [x] Check static build
+  ;;   - [x] Make xref work pointing to it
+  ;; - [ ] Add footnotes
+  ;; - [ ] Add inline blocks
+  ;; - [ ] Navigation (bottom bar)
+  ;;   - [ ] Create navigation based on index
+  ;; - [ ] Search
+  ;; - [ ] Glossary
+  ;; - [ ] Make it work with Mobile
+  ;; - [ ] Make scroll remember its position when clicking back or when
+  ;;       reloading the same page
+  ;; - [ ] Remove `Generated with Clerk` from the static build
+  ;; - [ ] Breadcrumb
+  ;; - [ ] For xref, refer to an marker in a file
+  ;; - [ ] PDF version?
+  ;; - [ ] Ebook version?
+  ;; - [ ] Support comment
+  ;; - [ ] Create Clerk text game
+  ;; - [ ] Fix flashing font
 
   ())
